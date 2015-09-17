d470b8ae1c320faed0e8dec3a2ada483e94167ed
Junyu <zhujunyu.njcn@gmail.com>
2015-09-17 00:32:51 -0400

    remove too large files

--- datasets_java/src/datasets/Main.java
+++ datasets_java/src/datasets/Main.java
@@ -23,36 +23,43 @@ public class Main {
 		MyPage[] myPageArray = new MyPage[sizeMyPage];
 		Friends[] friendsArray = new Friends[sizeFriends];
 		AccessLog[] accessLogArray = new AccessLog[sizeAccessLog];
-		// new
-		for(int i = 1; i <= sizeMyPage; i++) {
-			myPageArray[i - 1] = new MyPage(i);
-			//			System.out.println(myPageArray[i - 1].toString());
-		}
-		for(int i = 1; i <= sizeFriends; i++) {
-			friendsArray[i - 1] = new Friends(i);
-			//			System.out.println(friendsArray[i - 1].toString());
-		}
-		for(int i = 1; i <= sizeAccessLog; i++) {
-			accessLogArray[i - 1] = new AccessLog(i);
-			//			System.out.println(accessLogArray[i - 1].toString());
-		}
-		// write to file
-//		writer = new PrintWriter("../datasets/" + fileMyPage);
-//		for(int i = 0; i < myPageArray.length; i++) {
-//			writer.println(myPageArray[i].toString());
-//		}
-//		writer.close();
-//		writer = new PrintWriter("../datasets/" + fileFriends);
-//		for(int i = 0; i < friendsArray.length; i++) {
-//			writer.println(friendsArray[i].toString());
-//		}
-//		writer.close();
-//		writer = new PrintWriter("../datasets/" + fileAccessLog);
-//		for(int i = 0; i < accessLogArray.length; i++) {
-//			writer.println(accessLogArray[i].toString());
-//		}
-//		writer.close();
-		System.out.println("datasets files are created!");
+
+		/* 
+		 * mypage
+		 */
+		//		for(int i = 1; i <= sizeMyPage; i++) {
+		//			myPageArray[i - 1] = new MyPage(i);
+		//		}
+		//		writer = new PrintWriter("../datasets/" + fileMyPage);
+		//		for(int i = 0; i < myPageArray.length; i++) {
+		//			writer.println(myPageArray[i].toString());
+		//		}
+		//		writer.close();
+
+		/* 
+		 * friends
+		 */
+		//		for(int i = 1; i <= sizeFriends; i++) {
+		//			friendsArray[i - 1] = new Friends(i);
+		//		}
+		//		writer = new PrintWriter("../datasets/" + fileFriends);
+		//		for(int i = 0; i < friendsArray.length; i++) {
+		//			writer.println(friendsArray[i].toString());
+		//		}
+		//		writer.close();
+
+		/* 
+		 * accesslog
+		 */
+		//		for(int i = 1; i <= sizeAccessLog; i++) {
+		//			accessLogArray[i - 1] = new AccessLog(i);
+		//		}
+		//		writer = new PrintWriter("../datasets/" + fileAccessLog);
+		//		for(int i = 0; i < accessLogArray.length; i++) {
+		//			writer.println(accessLogArray[i].toString());
+		//		}
+		//		writer.close();
+		//		System.out.println("datasets files are created!");
 	}
 
 }
